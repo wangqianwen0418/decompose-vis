d2c30403b5bb02d8a6f1533d887b3441e6a0fcef
李振 <lizhen@lizhendeMacBook-Pro.local>
2017-01-09 16:33:57 +0800

    the initial version

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+	<vis-figure src="/assets/paper1.png" width="640" height="360"></vis-figure>
+  </div>
+</template>
+
+<script>
+import VisFigure from './VisFigure.vue';
+
+export default { components: { "vis-figure": VisFigure } };
+
+</script>
+
+<style>
+	html {
+		height: 100%;
+	}
+
+	body {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		height: 100%;
+	}
+
+	#app {
+		color: #2c3e50;
+		left: 10px;
+		top: 10px;
+		font-family: Source Sans Pro, Helvetica, sans-serif;
+		text-align: center;
+	}
+
+	#app a {
+		color: #42b983;
+		text-decoration: none;
+	}
+
+	.logo {
+		width: 100px;
+		height: 100px
+	}
+</style>
