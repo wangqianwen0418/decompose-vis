dfacda00b7739821fc9952c12c5e0e52838921ab
wangqianwen0418 <wangqianwen0418@163.com>
2017-01-12 22:05:07 +0800

    clips

---
+++ src/components/headline.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="headline">
+        vis 2017
+    </div>
+</template>
+
+<style scoped>
+ .headline{
+    min-height: 60px;
+    border-radius: 8px;
+    margin:0px 10px;
+    font-family: Arial, Helvetica, sans-serif;
+    font-size:40px;
+    background: #d3dce6;
+    color:white;
+  }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
-import App from './App.vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-default/index.css'
+import App from './App1.vue'
+
+Vue.use(ElementUI)
 
 new Vue({
   el: '#app',
