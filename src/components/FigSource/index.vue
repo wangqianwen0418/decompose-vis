d6bc581842a796b282c79b44fe29cff762ca5b3a
wangqianwen0418 <wangqianwen0418@163.com>
2017-03-16 17:03:30 +0800

    aa

--- src/components/FigSource/index.vue
+++ src/components/FigSource/index.vue
@@ -1,9 +1,4 @@
 <template src="./template.html"></template>
-<<<<<<< HEAD
 <script src="./script.js">
-
 </script>
-=======
-<script src="./script.js"></script>
->>>>>>> dev
 <style scoped src="./style.css"></style>
